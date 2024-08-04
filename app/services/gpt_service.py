523938ff144f1dc6efd4de57b67a924744fc4da7
0703kyj <kyj0703@konkuk.ac.kr>
2024-08-04 16:33:07 +0900

    infra: 오류 fix

--- app/services/gpt_service.py
+++ app/services/gpt_service.py
@@ -1,6 +1,6 @@
 import openai
 import logging
-from app.models import get_gpt_model
+from app.models.GPT import init
 
 # 로깅 설정
 logger = logging.getLogger(__name__)
@@ -10,7 +10,7 @@ def generate_response(prompt, max_tokens=300, temperature=0.7):
     GPT 모델을 사용하여 프롬프트에 대한 응답을 생성합니다.
     """
     try:
-        model = get_gpt_model()
+        model = init.get_gpt_model()
         response = openai.ChatCompletion.create(
             model=model,
             messages=[
